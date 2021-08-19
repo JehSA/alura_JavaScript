2065e8df2e6f6a4bd770e201b9b143663d09a737
Jefferson Almeida <jefferson.almeida@global.ntt>
2021-08-19 09:52:09 -0300

    codigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numeroString = "456";
+// const numeroString = Number("456");
+
+
+console.log(numero === numeroString);
+console.log(numero == numeroString);
+
+
+//conversão explicita
+// number()
+// string()
+
+console.log(numero + Number(numeroString));
+console.log(numero + numeroString);
